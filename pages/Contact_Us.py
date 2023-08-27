b64e8eb35f4fd94785289da758b8125c3724a44f
Jay Sim <yourthought2020@gmail.com>
2023-08-26 21:53:23 -0700

    Modify contact-us.py to render actual email contents

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,15 +1,26 @@
 import streamlit as st
 import pandas
+from send_email import send_email
 
 df = pandas.read_csv("/Users/jaysim/Documents/Coding-Project/company-website-bonus/topics.csv")
 
+st.header("Contact me")
+topics_list = df["topic"].tolist()
 
-st.title("Contact me")
+with st.form(key="email_forms"):
+    user_email = st.text_input(label="Enter your email")
+    selected_topic = st.selectbox('What topic do you want to discuss?', topics_list)
+    raw_message = st.text_area(label="Enter your message")
+    message = f"""\
+Subject: New mail from {user_email}
 
-st.text_input(label="Enter your email")
-for index, row in df.iterrows():
-    topics_list = (row["topic"])
-    st.selectbox('What topic do you want to discuss?',
-    (row["topic"]))
-st.text_area(label="Your message here")
-st.button("Submit")
+From: {user_email}
+
+Topic: {selected_topic}
+
+Contents: {raw_message}
+"""
+    button = st.form_submit_button("Submit")
+    if button:
+        send_email(message)
+        st.info("Your email was successfully sent")
